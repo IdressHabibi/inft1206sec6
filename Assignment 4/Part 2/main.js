00800bf5e95a287b350ecee3a28145d6845d8749
IdressHabibi <idress.habibi@dcmail.ca>
2024-07-31 20:40:56 -0400

    Looping through images

--- Assignment 4/Part 2/main.js
+++ Assignment 4/Part 2/main.js
@@ -12,15 +12,20 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-const alttext = ['closeup of eye','fossil/stone','orchids','painting egyptian','brown moth']
+
 
 /* Declaring the alternative text for each image file */
-
+const alttext = ['closeup of eye','fossil/stone','orchids','painting egyptian','brown moth']
 /* Looping through images */
-
+for (const image of images) {
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
+newImage.addEventListener('click', imageclick => {
+    displayedImage.src = imageclick.target.src;
+    displayedImage.alt = imageclick.target.alt;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
